65ce3975736a1c95ba1881079edeae279d37c03e
ngocsonqs <ngocson_jp@yahoo.co.jp>
2017-06-07 22:25:38 +0900

    01. let - example source code

--- README.md
+++ README.md
@@ -15,3 +15,8 @@
 		- Khi khởi tạo ra thì chỉ hoạt động được trong phạm vi scope block
 	- **var:**
 		- Khi khởi tọa ra thì sẽ hoạt động trong phạm vi toàn cục của file(chỗ nào cũng có thể gọi được)
+
+> [**View code : let**]()
+
+- - -
+
--- playground.js
+++ playground.js
@@ -3,11 +3,38 @@
 /*============================
 	01. let
 ============================*/
-
-function theNotebook() {
-	let movie = "The Notebook";
-	return movie;
+function foo() {
+	let hello = "Hello World";
+	if ( true ) {
+		let hello = "Hello ES6";
+		console.log(hello);	// ①
+	}
+	console.log(hello);	// ②
 }
 
-console.log(theNotebook());	// ①　 : không có error và xuât ra : The Notebook
-console.log(movie);			// ②  : có error vì không thể tham chiếu đến biến movie
+foo();
+
+/*=======================
+
+■　①:
+------
+	▲　KẾT QUẢ:
+		Hello ES6
+
+	▲ GIẢI THÍCH:
+		- MẶC DÙ	: ta khai báo biến Hello ở ngoài if
+		- NHƯNG	: vào vòng if ta đã khai báo đè
+		- HƠN NỮA	: let chỉ sử dụng trong phạm vi scope block 
+		- NÊN	: khi in ra ở đâu thì nó sẽ lấy giá trị trong phạm vi scope block gần nhất( ở vd này là vòng if)
+
+■　②:
+------
+	▲　KẾT QUẢ:
+		Hello World
+
+	▲ GIẢI THÍCH:
+		- MẶC DÙ	: trong vòng if ta có khai báo lại biến hello
+		- NHƯNG	: biến hello trong vòng if chỉ hoạt động ở trong phạm vi scope block đó thôi
+		- HƠN NỮA	: khi gọi biến đó nó sẽ xem trong phạm vi scope block của nó có biến đó không
+		- NÊN	: lần này nó không thể nhìn được vào trong phạm vi scope block if nên lấy giá trị hello ở ngoài để in ra
+========================*/
