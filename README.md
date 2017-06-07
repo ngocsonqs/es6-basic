6f956b7d569a397a0f6897893eeea055d74c68a1
ngocsonqs <ngocson_jp@yahoo.co.jp>
2017-06-07 22:31:15 +0900

    01. var - example var

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 	- **var:**
 		- Khi khởi tọa ra thì sẽ hoạt động trong phạm vi toàn cục của file(chỗ nào cũng có thể gọi được)
 
-> [**View code : let**]()
+> [**View code : let**](https://github.com/ngocsonqs/es6-basic/blob/65ce3975736a1c95ba1881079edeae279d37c03e/playground.js)
 
 - - -
 
--- playground.js
+++ playground.js
@@ -1,12 +1,12 @@
 "use strict";
 
 /*============================
-	01. let
+	01. var
 ============================*/
 function foo() {
-	let hello = "Hello World";
+	var hello = "Hello World";
 	if ( true ) {
-		let hello = "Hello ES6";
+		var hello = "Hello ES6";
 		console.log(hello);	// ①
 	}
 	console.log(hello);	// ②
@@ -24,17 +24,12 @@ foo();
 	▲ GIẢI THÍCH:
 		- MẶC DÙ	: ta khai báo biến Hello ở ngoài if
 		- NHƯNG	: vào vòng if ta đã khai báo đè
-		- HƠN NỮA	: let chỉ sử dụng trong phạm vi scope block 
-		- NÊN	: khi in ra ở đâu thì nó sẽ lấy giá trị trong phạm vi scope block gần nhất( ở vd này là vòng if)
+		- HƠN NỮA	: var sử dụng trong phạm vi function
+		- NÊN	: khi in ra ở đâu thì nó sẽ lấy giá trị gần nhất trong phạm vi function đó
 
 ■　②:
 ------
 	▲　KẾT QUẢ:
-		Hello World
+		Hello ES6
 
-	▲ GIẢI THÍCH:
-		- MẶC DÙ	: trong vòng if ta có khai báo lại biến hello
-		- NHƯNG	: biến hello trong vòng if chỉ hoạt động ở trong phạm vi scope block đó thôi
-		- HƠN NỮA	: khi gọi biến đó nó sẽ xem trong phạm vi scope block của nó có biến đó không
-		- NÊN	: lần này nó không thể nhìn được vào trong phạm vi scope block if nên lấy giá trị hello ở ngoài để in ra
 ========================*/
